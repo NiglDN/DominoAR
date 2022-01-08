6ed0bb7ae027350f734fdb5cf000e7295f8b0265
NiglDN <nigl2010@live.at>
2022-01-08 15:25:33 +0100

    initial commit

---
+++ Assets/MyScripts/MyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyController : MonoBehaviour
+{
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            this.transform.Rotate(Vector3.up * Time.deltaTime * speed * 100);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            this.transform.Rotate(Vector3.down * Time.deltaTime * speed * 100);
+        }
+    }
+}
---
+++ Assets/MyScripts/MySpawnDominio.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySpawnDominio : MonoBehaviour
+{
+    public GameObject[] dominos;
+    private int currentDominoIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDominoIndex = Random.Range(0, dominos.Length - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnObject();
+        }
+    }
+
+    // spawns a domino at the position of the current Gameobject
+    void SpawnObject()
+    {
+        shuffleDomino();
+        // TODO: fix rotation
+        Instantiate(dominos[currentDominoIndex], new Vector3(transform.position.x, 0, transform.position.z), new Quaternion(90, transform.rotation.y, 0, -90));
+    }
+
+    // change current selected Domino
+    private void shuffleDomino()
+    {
+        currentDominoIndex++;
+        if (currentDominoIndex >= dominos.Length )
+        {
+            currentDominoIndex = 0;
+        }
+    }
+}
